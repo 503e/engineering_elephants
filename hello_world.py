49440fe76ba03033f7a27bf7eaf05c58ba197775
Oliver Martin <oliver.martin@avinc.com>
2025-08-08 23:34:41 -0700

    first commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+from pybricks.hubs import PrimeHub
+from pybricks.tools import wait
+
+# Set up all devices.
+prime_hub = PrimeHub()
+
+# The main program starts here.
+prime_hub.display.number(99)
+wait(1000)
+print('Hello, Pybricks!')
