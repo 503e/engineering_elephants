9889d6393a64922a0f6fb791a511251952d5f4e4
kyanmartin <kyanjmartin@gmail.com>
2025-09-09 20:14:48 -0700

    I made the main light on the hub flash red.

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,6 @@
 from pybricks.hubs import PrimeHub
 from pybricks.tools import wait
+from pybricks.parameters import Color
 
 # Set up all devices.
 prime_hub = PrimeHub()
@@ -8,3 +9,9 @@ prime_hub = PrimeHub()
 prime_hub.display.number(99)
 wait(1000)
 print('Hello, Pybricks!')
+def blink_red_light():
+	prime_hub.light.blink(Color.RED, [100, 100])
+
+blink_red_light()
+
+wait(10000)
