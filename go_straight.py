a1b0999d8646420a105f2d7fed20ca52e3849def
kyanmartin <kyanjmartin@gmail.com>
2025-09-14 16:48:55 -0700

    uses gyro to go straight.

---
+++ go_straight.py
@@ -0,0 +1,16 @@
+from pybricks.hubs import PrimeHub
+from pybricks.tools import wait
+from pybricks.parameters import Color
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Port, Direction, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+left_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.B, Direction.CLOCKWISE)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
+
+robot.use_gyro(True)
+robot.straight(distance=1000, then=Stop.HOLD, wait=True)
