7fbbb60d9ddc488ab0ac6a2fe05546915b42be2e
gallagherluke-ops <gallagher.luke@gmail.com>
2025-10-08 20:46:54 -0700

    Quintens changes

---
+++ Menu_Control.py
@@ -0,0 +1,12 @@
+from pybricks.tools import hub_menu
+
+# Make a menu to choose a letter. You can also use numbers.
+selected = hub_menu("1", "2", "3")
+
+# Based on the selection, run a program.
+if selected == "1":
+    import hello_world
+elif selected == "2":
+    import sound
+elif selected == "3":
+    import xBox_Controls
