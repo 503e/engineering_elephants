df5cf0c6e76f7e3a347a6d531f8e5f53777599df
gallagherluke-ops <gallagher.luke@gmail.com>
2025-10-22 19:42:32 -0700

    updates including menu update for 4 mission runs

--- Menu_Control.py
+++ Menu_Control.py
@@ -1,12 +1,14 @@
 from pybricks.tools import hub_menu
 
 # Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu("1", "2", "3")
+selected = hub_menu("1", "2", "3", "4")
 
 # Based on the selection, run a program.
 if selected == "1":
     import mission_01
 elif selected == "2":
-    import sound
+    import thesandmission
 elif selected == "3":
-    import xBox_Controls
+    import mission_03
+elif    selected == "4":
+    import mission_02_final
--- mission_03.py
+++ mission_03.py
@@ -6,18 +6,20 @@ from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.parameters import Port, Direction, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from pybricks.iodevices import XboxController
 
 left_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B, Direction.CLOCKWISE)
-
+main_motor = Motor(Port.F, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
-
 robot.use_gyro(True)
-robot.straight(distance=800, then=Stop.HOLD, wait=True)
-robot.arc(radius=-50, angle=60, wait=True)
-robot.straight(distance=50, then=Stop.HOLD, wait=True)
-robot.straight(distance=-50, then=Stop.HOLD, wait=True)
-robot.arc(radius=-50, angle=-60, wait=True)
+main_motor.run_angle(600, -1000, then=Stop.HOLD, wait=True)#move attachment down
+robot.straight(distance=-700, then=Stop.HOLD, wait=True)
+robot.turn(90, wait=True)
+robot.straight(distance=-350, then=Stop.HOLD, wait=True)
+robot.turn(-70, wait=True)
+main_motor.run_angle(600, 900, then=Stop.HOLD, wait=True)#lift
+robot.turn(67, wait=True)
 robot.straight(distance=-800, then=Stop.HOLD, wait=True)
-#robot.straight(distance=-500, then=Stop.HOLD, wait=True)
-#robot.arc(500, angle=90, then=Stop.HOLD, wait=True)
+robot.arc(radius=-200, angle=-70, wait=True)
+robot.straight(distance=-800, then=Stop.HOLD, wait=True)
