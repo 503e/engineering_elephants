8943b2319d83bc1763a40d68cd3714c350faaf7d
gallagherluke-ops <gallagher.luke@gmail.com>
2025-11-05 21:35:26 -0800

    Adjusted the speed of mission 3 to shorten run time.

--- mission_03.py
+++ mission_03.py
@@ -12,7 +12,7 @@ left_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B, Direction.CLOCKWISE)
 main_motor = Motor(Port.F, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
-DriveBase.settings()
+robot.settings(straight_speed=750,straight_acceleration=600, turn_rate=500, turn_acceleration=350)#adjusts the speed that the robot runs at
 robot.use_gyro(True)
 main_motor.run_angle(600, -1000, then=Stop.HOLD, wait=True)#move attachment down
 robot.straight(distance=-700, then=Stop.HOLD, wait=True)
