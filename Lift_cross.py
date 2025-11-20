d32b588d2f09f2f2344a178ef9230e1725585e2d
gallagherluke-ops <gallagher.luke@gmail.com>
2025-11-19 18:41:20 -0800

    New lift + cross

--- Lift_cross.py
+++ Lift_cross.py
@@ -13,4 +13,38 @@ right_motor = Motor(Port.B, Direction.CLOCKWISE)
 main_motor = Motor(Port.F, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=112)
 robot.use_gyro(True)
-robot.straight(distance=-70, then=Stop.HOLD, wait=True)
+main_motor.run_angle(600, -720, then=Stop.HOLD, wait=False)#move attachment down
+robot.straight(distance=-480, then=Stop.HOLD,)
+robot.turn(angle=-90, wait=True)
+robot.straight(distance=-100, then=Stop.HOLD)
+main_motor.run_angle(600, 400, then=Stop.HOLD, wait=True)
+robot.turn(angle=90, wait=True)
+main_motor.run_angle(600, -400, then=Stop.HOLD, wait=False)
+wait(250)
+robot.straight(distance=-160, then=Stop.HOLD,)
+main_motor.run_angle(600, 720, then=Stop.HOLD, wait=True)
+robot.turn(angle=130, wait=True)
+robot.straight(distance=-350, then=Stop.HOLD,)
+main_motor.run_angle(600, -800, then=Stop.HOLD, wait=True)
+robot.straight(distance=200, then=Stop.HOLD,)
+main_motor.run_angle(600, -100, then=Stop.HOLD, wait=False)
+robot.turn(angle=-57, wait=True)
+robot.straight(distance=-270, wait=True)
+main_motor.run_angle(600, 700, then=Stop.HOLD, wait=True)#Minecart lift
+main_motor.run_angle(600, -1200, then=Stop.HOLD, wait=False)
+robot.turn(angle=75, wait=True)
+robot.straight(distance=-225, wait=True)
+main_motor.run_angle(600, 300, then=Stop.HOLD)
+robot.turn(angle=-25, wait=True)
+robot.straight(distance=100, wait=True)
+robot.turn(angle=-45, wait=True)
+robot.straight(distance=-600, wait=True)
+robot.arc(radius=-500, angle=-95)
+robot.straight(distance=-200)
+
+
+
+
+
+
+
